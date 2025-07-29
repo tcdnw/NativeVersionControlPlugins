6d06e0fe84aa18beb142fb8473dfe11faac28ffd
Sébastien Rombauts <sebastien.rombauts@unity3d.com>
2025-07-29 10:03:46 +0200

    Update plugin version to 1.3.0
    2025/07/29 P4 API 2021.2 for Perforce Cloud

--- P4Plugin/Source/P4PluginVersion.h
+++ P4Plugin/Source/P4PluginVersion.h
@@ -11,4 +11,6 @@
 //       2023.1.0a14
 // 1.2.1 2024/03/15 Fix login failure license about to expire
 //       2021.3.38f1, 2023.2.19f1, 6000.0.0b14
-#define PERFORCE_PLUGIN_VERSION "1.2.1"
+// 1.3.0 2025/07/29 P4 API 2021.2 for Perforce Cloud
+
+#define PERFORCE_PLUGIN_VERSION "1.3.0"
